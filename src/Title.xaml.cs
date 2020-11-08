39dbf439c884b7874c83efec0ff353389d0d20e6
EEL733 <nanbonmo@gmail.com>
2020-11-08 22:36:35 +0900

    タイトル画面を追加

---
+++ src/Title.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace JOYLAND {
+    public partial class Title : Window {
+        public Title() {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e) {
+            UserListWindow window = new UserListWindow();
+            window.Show();
+        }
+
+        private void ResultButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
