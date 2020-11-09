dd20f71b17709cf2495e923970dda63d86c18a81
EEL733 <nanbonmo@gmail.com>
2020-11-10 03:00:06 +0900

    副賞用データ追加

--- src/Model/MusicData.cs
+++ src/Model/MusicData.cs
@@ -7,8 +7,12 @@
         public int group { get; set; }
         public bool select { get; set; }
         public bool visible { get; set; }
+        public int bpm { get; set; }
+        public int fx { get; set; }
+        public int version { get; set; }
+        public bool touhou { get; set; }
 
-        public MusicData(int id, string name, string difficultyType, int level, int group, bool visible) {
+        public MusicData(int id, string name, string difficultyType, int level, int group, bool visible, int bpm, int fx, int version, bool touhou) {
             this.id = id;
             this.name = name;
             this.difficultyType = difficultyType;
@@ -16,6 +20,10 @@
             this.group = group;
             select = false;
             this.visible = visible;
+            this.bpm = bpm;
+            this.fx = fx;
+            this.version = version;
+            this.touhou = touhou;
         }
     }
 }
